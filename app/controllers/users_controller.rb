1ec56a5ca60977cfcdb2047ed8c5c2354ed6329e
223yu <223yuta.jp@gmail.com>
2021-07-06 16:38:04 +0000

    [Update]7a books sort at users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.sort { |a,b| b.a_week_favorited_count <=> a.a_week_favorited_count }
     @book = Book.new
   end
 
